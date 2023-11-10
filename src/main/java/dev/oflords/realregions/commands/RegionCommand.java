eab6fb2fea4e83d5e3ddbe5c19b54f9a63a8e267
oflords <oflordsaus@gmail.com>
2023-11-10 22:33:07 +0800

    Finished commands

--- src/main/java/dev/oflords/realregions/commands/RegionCommand.java
+++ src/main/java/dev/oflords/realregions/commands/RegionCommand.java
@@ -2,8 +2,10 @@ package dev.oflords.realregions.commands;
 
 import dev.oflords.realregions.RegionPlayer;
 import dev.oflords.realregions.region.Region;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -21,77 +23,105 @@ public class RegionCommand implements CommandExecutor, TabCompleter {
     // This method is called, when somebody uses our command
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
-
+        if (sender instanceof Player player) {
             if (args.length == 0) {
                 openMenu(player);
                 return true;
             } else if (args.length == 1) {
-                if (args[0].equals("wand")) {
-                    ItemStack wand = new ItemStack(Material.GOLDEN_SHOVEL);
-                    ItemMeta wandMeta = wand.getItemMeta();
-                    wandMeta.setDisplayName(ChatColor.YELLOW + "Region Wand");
-                    wand.setItemMeta(wandMeta);
-                    player.getInventory().addItem(wand);
-
-                    sender.sendMessage(ChatColor.GREEN + "You have been given a Region Wand!");
-                } else if (args[0].equals("create")) {
-                    sender.sendMessage("Usage: /region create <name>");
-                } else if (args[0].equals("whitelist")) {
-                    sender.sendMessage("Usage: /region whitelist <name>");
-                } else if (args[0].equals("add")) {
-                    sender.sendMessage("Usage: /region add <name> <player>");
-                } else if (args[0].equals("remove")) {
-                    sender.sendMessage("Usage: /region remove <name> <player>");
-                } else {
-                    openMenu(player);
-                    return true;
+                switch (args[0]) {
+                    case "wand" -> {
+                        ItemStack wand = new ItemStack(Material.GOLDEN_SHOVEL);
+                        ItemMeta wandMeta = wand.getItemMeta();
+                        wandMeta.setDisplayName(ChatColor.YELLOW + "Region Wand");
+                        wand.setItemMeta(wandMeta);
+                        player.getInventory().addItem(wand);
+                        sender.sendMessage(ChatColor.GREEN + "You have been given a Region Wand!");
+                    }
+                    case "create" -> sender.sendMessage("Usage: /region create <name>");
+                    case "whitelist" -> sender.sendMessage("Usage: /region whitelist <name>");
+                    case "add" -> sender.sendMessage("Usage: /region add <name> <player>");
+                    case "remove" -> sender.sendMessage("Usage: /region remove <name> <player>");
+                    default -> {
+                        openMenu(player);
+                        return true;
+                    }
                 }
             } else if (args.length == 2) {
                 String name = args[1];
-                if (args[0].equals("create")) {
-                    for (Region region : Region.regions) {
-                        if (region.getOwner() == player.getUniqueId() && region.getName().toLowerCase().equals(name.toLowerCase())) {
-                            player.sendMessage(ChatColor.RED + "You already have a region named " + name);
-                            return false;
-                        }
-                    }
 
-                    RegionPlayer regionPlayer = RegionPlayer.getByUUID(player.getUniqueId());
-                    if (regionPlayer == null || regionPlayer.getPos1() == null || regionPlayer.getPos2() == null) {
-                        player.sendMessage(ChatColor.RED + "You must use /region wand and set two locations before creating a region!");
-                        return false;
-                    }
-
-                    new Region(name, player.getUniqueId(), regionPlayer.getPos1(), regionPlayer.getPos2());
-                    player.sendMessage(ChatColor.GREEN + "Created new region " + name + "!");
-                } else if (args[0].equals("whitelist")) {
-                    for (Region region : Region.regions) {
-                        if (region.getOwner() == player.getUniqueId() && region.getName().toLowerCase().equals(name.toLowerCase())) {
-                            player.sendMessage(ChatColor.AQUA + name + " Whitelist:");
-                            if (region.getWhitelist().isEmpty()) {
-                                player.sendMessage("- Nobody");
-                            } else {
-                                for (UUID uuid : region.getWhitelist()) {
-                                    player.sendMessage("- " + uuid);
-                                }
+                switch (args[0]) {
+                    case "create" -> {
+                        for (Region region : Region.regions) {
+                            if (region.getOwner() == player.getUniqueId() && region.getName().toLowerCase().equals(name.toLowerCase())) {
+                                player.sendMessage(ChatColor.RED + "You already have a region named " + name);
+                                return false;
                             }
+                        }
+                        RegionPlayer regionPlayer = RegionPlayer.getByUUID(player.getUniqueId());
+                        if (regionPlayer == null || regionPlayer.getPos1() == null || regionPlayer.getPos2() == null) {
+                            player.sendMessage(ChatColor.RED + "You must use /region wand and set two locations before creating a region!");
                             return false;
                         }
+                        new Region(name, player.getUniqueId(), regionPlayer.getPos1(), regionPlayer.getPos2());
+                        player.sendMessage(ChatColor.GREEN + "Created new region " + name + "!");
                     }
-
-                    player.sendMessage(ChatColor.RED + "Could not find region named" + name);
-                } else if (args[0].equals("add")) {
-                    sender.sendMessage("Usage: /region add <name> <player>");
-                } else if (args[0].equals("remove")) {
-                    sender.sendMessage("Usage: /region remove <name> <player>");
+                    case "whitelist" -> {
+                        for (Region region : Region.regions) {
+                            if (region.getOwner() == player.getUniqueId() && region.getName().toLowerCase().equals(name.toLowerCase())) {
+                                player.sendMessage(ChatColor.AQUA + name + " Whitelist:");
+                                if (region.getWhitelist().isEmpty()) {
+                                    player.sendMessage("- Nobody");
+                                } else {
+                                    for (UUID uuid : region.getWhitelist()) {
+                                        player.sendMessage("- " + uuid);
+                                    }
+                                }
+                                return false;
+                            }
+                        }
+                        player.sendMessage(ChatColor.RED + "Could not find region named" + name);
+                    }
+                    case "add" -> sender.sendMessage("Usage: /region add <name> <player>");
+                    case "remove" -> sender.sendMessage("Usage: /region remove <name> <player>");
                 }
             } else if (args.length == 3) {
+                String name = args[1];
+
                 if (args[0].equals("add")) {
-                    sender.sendMessage("add");
+                    for (Region region : Region.regions) {
+                        if (region.getOwner() == player.getUniqueId() && region.getName().toLowerCase().equals(name.toLowerCase())) {
+                            OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(args[2]);
+                            if (offlinePlayer == null) {
+                                player.sendMessage(ChatColor.RED + "Could not find player...");
+                                return false;
+                            }
+                            if (region.getWhitelist().contains(offlinePlayer.getUniqueId())) {
+                                player.sendMessage(ChatColor.RED + "Player has already been added...");
+                                return false;
+                            }
+                            region.getWhitelist().add(offlinePlayer.getUniqueId());
+                            player.sendMessage(ChatColor.GREEN + "Added " + offlinePlayer.getName() + " to your region whitelist!");
+                            return false;
+                        }
+                    }
+                    player.sendMessage(ChatColor.RED + "Could not find region named" + name);
                 } else if (args[0].equals("remove")) {
-                    sender.sendMessage("remove");
+                    for (Region region : Region.regions) {
+                        if (region.getOwner() == player.getUniqueId() && region.getName().toLowerCase().equals(name.toLowerCase())) {
+                            OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(args[2]);
+                            if (offlinePlayer == null) {
+                                player.sendMessage(ChatColor.RED + "Could not find player...");
+                                return false;
+                            }
+                            if (!region.getWhitelist().contains(offlinePlayer.getUniqueId())) {
+                                player.sendMessage(ChatColor.RED + "Player has not been added...");
+                                return false;
+                            }
+                            region.getWhitelist().add(offlinePlayer.getUniqueId());
+                            player.sendMessage(ChatColor.GREEN + "Removed " + offlinePlayer.getName() + " from your region whitelist!");
+                            return false;
+                        }
+                    }
                 }
             }
         }
