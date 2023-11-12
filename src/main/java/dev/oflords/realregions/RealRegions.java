bff3199a1d9ed10d9835790e21be6f14aab124e3
oflords <oflordsaus@gmail.com>
2023-11-12 15:28:39 +0800

    SQL Support

--- src/main/java/dev/oflords/realregions/RealRegions.java
+++ src/main/java/dev/oflords/realregions/RealRegions.java
@@ -2,19 +2,27 @@ package dev.oflords.realregions;
 
 import dev.oflords.realregions.commands.RegionCommand;
 import dev.oflords.realregions.region.RegionListener;
+import dev.oflords.realregions.util.SQL;
 import dev.oflords.realregions.util.menu.MenuListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RealRegions extends JavaPlugin {
     public static RealRegions realRegions;
+    public SQL sql;
     @Override
     public void onEnable() {
         this.getCommand("region").setExecutor(new RegionCommand());
 
+        this.sql = new SQL(getConfig());
+        this.sql.load();
+
         getServer().getPluginManager().registerEvents(new RegionListener(), this);
         getServer().getPluginManager().registerEvents(new MenuListener(), this);
+    }
 
-        // TODO: Load regions from SQL
+    @Override
+    public void onDisable() {
+        this.sql.save();
     }
 
     public static RealRegions get() {
--- src/main/java/dev/oflords/realregions/util/SQL.java
+++ src/main/java/dev/oflords/realregions/util/SQL.java
@@ -1,10 +1,40 @@
 package dev.oflords.realregions.util;
 
+import dev.oflords.realregions.RealRegions;
 import dev.oflords.realregions.region.Region;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.UUID;
 
 public class SQL {
+
+    private Connection connection;
+
+    private String address;
+    private String database;
+    private int port;
+    private String username;
+    private String password;
+
+    public SQL(FileConfiguration config) {
+        if (config.isConfigurationSection("database")) {
+            ConfigurationSection configCursor = config.getConfigurationSection("database");
+            this.address = configCursor.getString("address");
+            this.database = configCursor.getString("database");
+            this.port = configCursor.getInt("port"); // 3306
+            this.username = configCursor.getString("username");
+            this.password = configCursor.getString("password");
+
+            this.openConnection();
+            // if (this.loaded) this.createTable();
+        }
+    }
+
     // Main Table Structure:
     // ID - int (auto increment, primary key)
     // Name - varchar(100)
@@ -19,6 +49,42 @@ public class SQL {
     // RegionID - int
     // User - UUID
     // Active - bool
+
+    private void openConnection() {
+        try {
+            // The newInstance() call is a work around for some
+            // broken Java implementations
+
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+        } catch (Exception ex) {
+            // handle the error
+        }
+
+        try {
+            this.connection = DriverManager.getConnection("jdbc:mysql://" + this.address + ":" + this.port + "/" + this.database + "?autoReconnect=true", this.username, this.password.isEmpty() ? null : this.password);
+            // this.connection = DriverManager.getConnection("jdbc:mysql://" + this.address + ":" + this.port + "/" + this.database + ";useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true", this.username, this.password.isEmpty() ? null : this.password);
+            RealRegions.get().getLogger().info("[Database] Connection to the database was successful.");
+        } catch (SQLException ex) {
+            RealRegions.get().getLogger().info("[Database] Could not establish a connection to a database... Shutting down");
+            RealRegions.get().getLogger().info("[Database] Reason: " + ex.getMessage());
+
+            Bukkit.getPluginManager().disablePlugin(RealRegions.get());
+            Bukkit.getServer().shutdown();
+        }
+    }
+
+    public Connection getConnection() {
+        if (connection == null) openConnection();
+        return connection;
+    }
+
+    public void closeConnection() {
+        try {
+            if (this.connection != null && !connection.isClosed()) this.connection.close();
+        } catch (SQLException ex) {
+//            ex.printStackTrace();
+        }
+    }
 
     public void save() {
         for (Region region : Region.regions) {
